9bdf33e9b647c60fa2901eac92f77f52b2ff805a
bilalbentoumi <bilalbentoumi@gmail.com>
2022-03-20 18:26:13 +0100

    Add dummy data for developers widget

--- src/components/FollowDevelops.jsx
+++ src/components/FollowDevelops.jsx
@@ -1,25 +1,29 @@
+import developers from '../data/developers.json'
+
 function FollowDevelopers() {
-    
+
     return (
         <div className="bg-white shadow rounded-md mb-4 h-max sticky top-20">
 
-            <div className="px-6 py-4">
+            <div className="px-6 py-4 pb-2">
                 <h2 className="text-lg font-semibold text-gray-800">Follow Developers</h2>
             </div>
 
             <div className="flex flex-col px-6 divide-y divide-gray-100">
-                { [...Array(5)].map(() => (
-                    <div className="flex items-center gap-2 py-4">
+
+                { developers?.map((developer, index) => (
+                    <div className="flex items-center gap-2 py-4" key={ index }>
                         <a href="">
-                            <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="" />
+                            <img className="h-10 w-10 rounded-full" src={ '/src/assets/avatar/' + developer.picture } alt={ developer.name } />
                         </a>
                         <div className="flex flex-col items-start flex-grow">
-                            <a href="">Bilal Bentoumi</a>
-                            <span className="text-sm text-gray-500">12 posts</span>
+                            <a href="">{ developer.name }</a>
+                            <span className="text-sm text-gray-500">{ developer.posts + ' posts'}</span>
                         </div>
                         <button className="text-xs font-semibold text-gray-400 px-3 py-1.5 rounded-md hover:text-white hover:bg-indigo-600 duration-150">FOLLOW</button>
                     </div>
                 )) }
+
             </div>
 
         </div>
