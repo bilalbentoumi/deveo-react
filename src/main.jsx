09dd5c61f0cc525a35cafbd3c50a61f0ba12357d
bilalbentoumi <bilalbentoumi@gmail.com>
2022-04-04 14:18:40 +0100

    Fix router basename

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './assets/css/index.css'
 import App from './App'
 
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <BrowserRouter basename="/deveo-react">
+    <BrowserRouter basename={ isDev ? '/' : '/deveo-react/' }>
         <App />
     </BrowserRouter>
 )
